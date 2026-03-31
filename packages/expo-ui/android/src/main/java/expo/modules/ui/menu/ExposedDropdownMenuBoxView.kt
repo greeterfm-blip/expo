3287e9ddb260edad34ae77c4420060672aa8e00a
Vojtech Novak <vonovak@gmail.com>
2026-03-31 14:02:54 +0200

    address feedback

---
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/menu/ExposedDropdownMenuBoxView.kt
@@ -0,0 +1,51 @@
+package expo.modules.ui.menu
+
+import android.graphics.Color
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.runtime.Composable
+import expo.modules.kotlin.views.ComposeProps
+import expo.modules.kotlin.views.FunctionalComposableScope
+import expo.modules.ui.ModifierList
+import expo.modules.ui.ModifierRegistry
+import expo.modules.ui.composeOrNull
+import expo.modules.ui.findChildSlotView
+import expo.modules.ui.renderSlot
+
+data class ExposedDropdownMenuBoxProps(
+  val expanded: Boolean = false,
+  val enabled: Boolean = true,
+  val menuContainerColor: Color? = null,
+  val modifiers: ModifierList = emptyList()
+) : ComposeProps
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FunctionalComposableScope.ExposedDropdownMenuBoxContent(
+  props: ExposedDropdownMenuBoxProps,
+  onExpandedChange: (Boolean) -> Unit
+) {
+  val itemsSlotView = findChildSlotView(view, "items")
+  val anchorSlotView = findChildSlotView(view, "anchor")
+
+  ExposedDropdownMenuBox(
+    expanded = props.expanded,
+    onExpandedChange = { if (props.enabled) onExpandedChange(it) },
+    modifier = ModifierRegistry.applyModifiers(props.modifiers, appContext, composableScope, globalEventDispatcher)
+  ) {
+    Box(modifier = androidx.compose.ui.Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)) {
+      anchorSlotView?.renderSlot()
+    }
+
+    ExposedDropdownMenu(
+      expanded = props.expanded,
+      onDismissRequest = { if (props.enabled) onExpandedChange(false) },
+      containerColor = props.menuContainerColor.composeOrNull ?: MenuDefaults.containerColor
+    ) {
+      itemsSlotView?.renderSlot()
+    }
+  }
+}
